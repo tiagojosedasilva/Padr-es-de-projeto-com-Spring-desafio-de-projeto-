bd8309412cbd809380983c185a77f1667dacb0bb
tiagojosedasilva <76395588+tiagojosedasilva@users.noreply.github.com>
2023-02-18 12:08:51 -0300

    Add files via upload

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/repositorys/ClienteRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.labpadroesprojetospring.repositorys;
+
+import one.digitalinnovation.labpadroesprojetospring.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/repositorys/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.labpadroesprojetospring.repositorys;
+
+import one.digitalinnovation.labpadroesprojetospring.model.Endereco;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
